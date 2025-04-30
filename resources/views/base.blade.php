8dedd37ae3009dcd44d6e690e93feebbe1714465
Fl4m3urDev <VoxFlameur22@gmail.com>
2025-04-30 11:41:21 +0200

    uninstall bootstrap and update tailwind css config

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,11 +8,11 @@
     <!-- CSRF Token -->
     <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
-    @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="bg-gray-900 text-white">
         @include('templates/header')
     </nav>
     <div class="container">
@@ -21,7 +21,6 @@
     <footer class="bg-dark text-center text-white py-3 mt-5">
         @include('templates/footer')
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
+<script src="//unpkg.com/alpinejs" defer></script>
 </html>
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,48 +1,48 @@
-<div class="container-fluid">
-  <a class="navbar-brand" href="{{ url('/') }}">Accueil</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<div class="container mx-auto px-4 py-3 flex flex-wrap items-center justify-between">
+    <a href="{{ url('/') }}" class="text-xl font-semibold text-white">Accueil</a>
 
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav me-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('vignettes.index') }}">Liste des Vignettes</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('vignettes.create') }}">Créer une Vignette</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav ms-auto">
-      @guest
-        @if (Route::has('login'))
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">Se connecter</a>
-          </li>
-        @endif
-        @if (Route::has('register'))
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">S'inscrire</a>
-          </li>
-        @endif
-      @else
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-            {{ Auth::user()->name }}
-          </a>
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li>
-              <a class="dropdown-item" href="{{ route('logout') }}"
-                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Déconnexion
-              </a>
-            </li>
-          </ul>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-          </form>
-        </li>
-      @endguest
-    </ul>
-  </div>
+    <button class="text-white md:hidden" @click="open = !open" x-data="{ open: false }" aria-label="Menu">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2"
+             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </button>
+
+    <div class="w-full md:flex md:items-center md:w-auto" :class="{ 'block': open, 'hidden': !open }" x-show="open || window.innerWidth >= 768">
+        <ul class="flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0">
+            <li><a href="{{ route('vignettes.index') }}" class="block py-2 text-white hover:text-gray-300">Liste des Vignettes</a></li>
+            <li><a href="{{ route('vignettes.create') }}" class="block py-2 text-white hover:text-gray-300">Créer une Vignette</a></li>
+        </ul>
+        <ul class="flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0 md:ml-auto">
+            @guest
+                @if (Route::has('login'))
+                    <li><a href="{{ route('login') }}" class="block py-2 text-white hover:text-gray-300">Se connecter</a></li>
+                @endif
+                @if (Route::has('register'))
+                    <li><a href="{{ route('register') }}" class="block py-2 text-white hover:text-gray-300">S'inscrire</a></li>
+                @endif
+            @else
+                <li class="relative" x-data="{ dropdown: false }">
+                    <button @click="dropdown = !dropdown" class="flex items-center py-2 text-white hover:text-gray-300">
+                        {{ Auth::user()->name }}
+                        <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd"
+                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414L10 13.414l-4.707-4.707a1 1 0 010-1.414z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </button>
+                    <div x-show="dropdown" @click.away="dropdown = false"
+                         class="absolute right-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg z-10">
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                           class="block px-4 py-2 hover:bg-gray-100">Déconnexion</a>
+                    </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                        @csrf
+                    </form>
+                </li>
+            @endguest
+        </ul>
+    </div>
 </div>
